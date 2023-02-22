e0647a9f2b7bb90f37f95f0b9dae06871e6a8496
randyahx <randyahx@gmail.com>
2023-02-22 13:29:31 +0800

    -

--- db/model/event_model.go
+++ db/model/event_model.go
@@ -6,11 +6,13 @@ import (
 
 type EventStartChallenge struct {
 	Id                int64
-	SpOperatorAddress string `gorm:"NOT NULL"`
-	ObjectId          uint64 `gorm:"NOT NULL"`
-	Index             int    `gorm:"NOT NULL"`
 	ChallengeId       uint64 `gorm:"NOT NULL"`
+	ObjectId          uint64 `gorm:"NOT NULL"`
+	SegmentIndex      uint32 `gorm:"NOT NULL"`
+	SpOperatorAddress string `gorm:"NOT NULL"`
+	RedundancyIndex   int32  `gorm:"NOT NULL"`
 	Height            uint64 `gorm:"NOT NULL;"`
+	Status            string `gorm:"NOT NULL;"`
 
 	//Height uint64 `gorm:"NOT NULL;index:idx_inscription_relay_transaction_height"`
 }
---
+++ vote/votepool_executor.go
@@ -0,0 +1,66 @@
+package vote
+
+import (
+	"context"
+	"github.com/gnfd-challenger/client/rpc"
+	"github.com/gnfd-challenger/config"
+
+	coretypes "github.com/tendermint/tendermint/rpc/core/types"
+	"github.com/tendermint/tendermint/rpc/jsonrpc/client"
+	"github.com/tendermint/tendermint/votepool"
+)
+
+type VotePoolExecutor struct {
+	client           *client.Client
+	config           *config.Config
+	greenfieldClient *rpc.GreenfieldChallengerClient
+}
+
+func NewVotePoolExecutor(cfg *config.Config, greenfieldClient *rpc.GreenfieldChallengerClient) *VotePoolExecutor {
+	cli, err := client.New(cfg.VotePoolConfig.RPCAddr)
+	if err != nil {
+		panic(err)
+	}
+	return &VotePoolExecutor{
+		client:           cli,
+		config:           cfg,
+		greenfieldClient: greenfieldClient,
+	}
+}
+
+func (e *VotePoolExecutor) QueryVotes(eventHash []byte, eventType votepool.EventType) ([]*votepool.Vote, error) {
+	queryMap := make(map[string]interface{})
+	queryMap[VotePoolQueryParameterEventType] = int(eventType)
+	queryMap[VotePoolQueryParameterEventHash] = eventHash
+	var queryVote coretypes.ResultQueryVote
+	_, err := e.client.Call(context.Background(), VotePoolQueryMethodName, queryMap, &queryVote)
+	if err != nil {
+		return nil, err
+	}
+	return queryVote.Votes, nil
+}
+
+func (e *VotePoolExecutor) BroadcastVote(v *votepool.Vote) error {
+	broadcastMap := make(map[string]interface{})
+	broadcastMap[VotePoolBroadcastParameterKey] = *v
+	var broadcastVote coretypes.ResultBroadcastVote
+
+	_, err := e.client.Call(context.Background(), VotePoolBroadcastMethodName, broadcastMap, &broadcastVote)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+//
+//func (e *VotePoolExecutor) GetValidatorsBlsPublicKey() ([]string, error) {
+//	validators, err := e.greenfieldClient.QueryLatestValidators()
+//	if err != nil {
+//		return nil, err
+//	}
+//	var keys []string
+//	for _, v := range validators {
+//		keys = append(keys, hex.EncodeToString(v.GetRelayerBlsKey()))
+//	}
+//	return keys, nil
+//}
