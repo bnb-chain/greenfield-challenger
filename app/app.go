11c1bcbc6ee2c0ea40253c8b42ec668bd7c080bd
forcodedancing <just.haha.it@gmail.com>
2023-03-01 20:22:52 +0800

    heartbeat: implement heartbeat, code refactor

--- app/app.go
+++ app/app.go
@@ -8,19 +8,24 @@ import (
 	"github.com/bnb-chain/greenfield-challenger/db/model"
 	"github.com/bnb-chain/greenfield-challenger/executor"
 	"github.com/bnb-chain/greenfield-challenger/monitor"
+	"github.com/bnb-chain/greenfield-challenger/vote"
+	ethcommon "github.com/ethereum/go-ethereum/common"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 type App struct {
-	m *monitor.Monitor
+	eventMonitor *monitor.Monitor
+
+	heartbeatProcessor *vote.VoteProcessor
 
 	//TODO: verifier
-	//TODO: heartbeat
+	//TODO: attest
 }
 
 func NewApp(cfg *config.Config) *App {
 	db, err := gorm.Open(mysql.Open(cfg.DBConfig.DBPath), &gorm.Config{})
+	db = db.Debug()
 	if err != nil {
 		panic(fmt.Sprintf("open db error, err=%s", err.Error()))
 	}
@@ -36,13 +41,25 @@ func NewApp(cfg *config.Config) *App {
 
 	executor := executor.NewExecutor(cfg)
 
-	m := monitor.NewMonitor(executor, daoManager)
+	monitor := monitor.NewMonitor(executor, daoManager)
+
+	signer := vote.NewVoteSigner(ethcommon.Hex2Bytes(cfg.VotePoolConfig.BlsPrivateKey))
+	votePoolExecutor := vote.NewVotePoolExecutor(cfg)
+
+	//TODO: config interval
+	heartbeat := vote.NewHeartbeatKind(daoManager, 100)
+	heartbeatProcessor := vote.NewVoteProcessor(cfg, daoManager, signer, executor, votePoolExecutor, heartbeat)
 
 	return &App{
-		m: m,
+		eventMonitor:       monitor,
+		heartbeatProcessor: heartbeatProcessor,
 	}
 }
 
 func (a *App) Start() {
-	a.m.StartLoop()
+	go a.eventMonitor.StartLoop()
+
+	// for heartbeat
+	go a.heartbeatProcessor.SignAndBroadcast()
+	a.heartbeatProcessor.CollectVotes()
 }
--- db/model/event.go
+++ db/model/event.go
@@ -13,12 +13,14 @@ type Event struct {
 	SegmentIndex      uint32      `gorm:"NOT NULL"`
 	SpOperatorAddress string      `gorm:"NOT NULL"`
 	RedundancyIndex   int32       `gorm:"NOT NULL"`
+	ChallengerAddress string      `gorm:"NOT NULL"`
 	Height            uint64      `gorm:"NOT NULL;"`
-	Status            EventStatus `gorm:"NOT NULL;"`
+	AttestStatus      EventStatus `gorm:"NOT NULL;"`
+	HeartbeatStatus   EventStatus `gorm:"NOT NULL;"`
 }
 
 func (*Event) TableName() string {
-	return "event"
+	return "events"
 }
 
 func InitEventTable(db *gorm.DB) {
@@ -30,17 +32,18 @@ func InitEventTable(db *gorm.DB) {
 	}
 }
 
-type EventStatus uint32
+type EventStatus int
 
 // Unprocessed for events that have not been challenged
 // ProcessedSucceed, ProcessedFailed for challenged events but not voted
 // VotedSucceed, VotedFailed for events that have been challenged AND voted
 const (
-	Unprocessed      EventStatus = 0
-	ProcessedSucceed EventStatus = 1
-	ProcessedFailed  EventStatus = 2
-	VotedSucceed     EventStatus = 3
-	VotedFailed      EventStatus = 4
+	Unprocessed              EventStatus = 0 // Event is just stored
+	VerifiedValidChallenge   EventStatus = 1 // Event has been verified, and the challenge is valid
+	VerifiedInvalidChallenge EventStatus = 2 // Event has been verified, and the challenge is invalid
+	SelfVoted                EventStatus = 3 // Event has been voted locally
+	AllVoted                 EventStatus = 4 // Event has been voted for more than 2/3 validators
+	Submitted                EventStatus = 5 // Event has been submitted for tx
 )
 
 func EventStatusToStr(status EventStatus) (string, error) {
