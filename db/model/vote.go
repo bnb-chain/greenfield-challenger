2355741fe534ecc96c119bfb62032a4bbaff98a8
randyahx <randyahx@gmail.com>
2023-03-24 14:15:19 +0800

    Update vote.go

--- db/model/vote.go
+++ db/model/vote.go
@@ -6,7 +6,8 @@ import (
 
 type Vote struct {
 	Id          int64
-	PubKey      []byte `gorm:"NOT NULL;index:idx_challenge_id_pub_key"`
+	ChallengeId uint64 `gorm:"NOT NULL;index:idx_challenge_id_pub_key"`
+	PubKey      []byte `gorm:"NOT NULL;index:idx_pub_key"`
 	Signature   string `gorm:"NOT NULL"`
 	EventType   uint32 `gorm:"NOT NULL"`
 	EventHash   []byte `gorm:"NOT NULL"`
