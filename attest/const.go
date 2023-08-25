ffa025c7e887d461c5ea8bf2d75447dd4fcaa190
randyahx <randyahx@gmail.com>
2023-08-25 09:41:51 +0800

    removed ants.pool

--- attest/const.go
+++ attest/const.go
@@ -3,6 +3,6 @@ package attest
 import "time"
 
 const (
-	QueryAttestedChallengeInterval = 5 * time.Second                                       // query last attested challenge id
+	QueryAttestedChallengeInterval = 10 * time.Second                                      // query last attested challenge id
 	MaxQueryCount                  = int((1 * time.Hour) / QueryAttestedChallengeInterval) // query last attested challenge id limit
 )
